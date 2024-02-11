8837c4e82b2c42ba8d90aa03e14ea26c167ae94c
unknown <khu0mmh@gmail.com>
2024-02-11 22:21:15 +0700

    Everything

---
+++ index.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var cors = require('cors');
+const multer  = require('multer');
+const upload = multer({ dest: 'uploads/' })
+require('dotenv').config()
+
+var app = express();
+
+app.use(cors());
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.get('/', function (req, res) {
+  res.sendFile(process.cwd() + '/views/index.html');
+});
+
+
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  })
+})
+
+const port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log('Your app is listening on port ' + port)
+});
